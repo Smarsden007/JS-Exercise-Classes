05cb2004ac308b187476dca7525808ba026ea62c
smarsden007 <you@example.com>
2022-02-17 18:58:52 -0800

    finsihed task 5

--- index.js
+++ index.js
@@ -164,9 +164,13 @@ grade(student, subject){
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor({name, age, location, previousBackground, className, favLanguage}){
+      super({name, age, location, previousBackground, className, favLanguage});
+    }
+  }
    
-}
+
 
 /*
   TASK 6
